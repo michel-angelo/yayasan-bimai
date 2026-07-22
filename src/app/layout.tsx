b4feeffa07a893520e2ec27864c50ea4d23c812f
Basthatan <panjiahmadbasthatanfialillmi@gmail.com>
2026-07-22 23:15:54 +0700

    feat(analytics): add Meta Pixel (ID 1023891886904648)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -98,6 +98,7 @@ export default function RootLayout({
   return (
     <html lang="id" className="scroll-smooth" suppressHydrationWarning>
       <head>
+        {/* TikTok Pixel */}
         <Script id="tiktok-pixel" strategy="afterInteractive">
           {`
             !function (w, d, t) {
@@ -105,6 +106,22 @@ export default function RootLayout({
               ttq.load('D9ECINRC77UBS5FSH6M0');
               ttq.page();
             }(window, document, 'ttq');
+          `}
+        </Script>
+
+        {/* Meta Pixel (Facebook Ads) */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1023891886904648');
+            fbq('track', 'PageView');
           `}
         </Script>
       </head>
